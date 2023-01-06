6f0899b6091df767e55aa673ad70cbd7c722a172
Sylvia McLaughlin <sylvia.mclaughlin@cds-snc.ca>
2023-01-06 12:15:42 -0800

    Adding additional files needed to run github action

--- src/action.py
+++ src/action.py
@@ -14,4 +14,4 @@ if __name__ == "__main__":
 	# call handle_log function to send data to Sentinel
         handle_log(fileName, inputData, log_analytics_workspace_id, log_analytics_workspace_key, "TestData_CL")
     except Exception as e:
-        rint(e)
+        print(e)
