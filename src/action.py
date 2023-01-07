419a992f28b02d43a14f4a17121117f98af28c0c
Sylvia McLaughlin <sylvia.mclaughlin@cds-snc.ca>
2023-01-06 16:39:30 -0800

    Adding .gitignore file and fixing up the devcontainers so that you can run the application properly in vscode

--- src/action.py
+++ src/action.py
@@ -7,11 +7,12 @@ if __name__ == "__main__":
     # get all the passed in variables
     fileName =os.environ["INPUT_FILE_NAME"]
     inputData =os.environ["INPUT_INPUT_DATA"]
+    logType =os.environ["INPUT_LOG_TYPE"] 
     log_analytics_workspace_id =os.environ["INPUT_LOG-ANALYTICS-WORKSPACE-ID"]
     log_analytics_workspace_key =os.environ["INPUT_LOG-ANALYTICS-WORKSPACE-KEY"]
     
     try:
 	# call handle_log function to send data to Sentinel
-        handle_log(fileName, inputData, log_analytics_workspace_id, log_analytics_workspace_key, "TestData_CL")
+        handle_log(fileName, inputData, log_analytics_workspace_id, log_analytics_workspace_key,logType) 
     except Exception as e:
         print(e)
